f521444bc9e15a81f35dec37153d557ef9369f1f
EndDotExe <hughiemck@icloud.com>
2025-04-08 04:18:02 +0000

    created a backup of plain_text.txt in case i delete something by accident. trying to get change password function to work

--- my_work/main.py
+++ my_work/main.py
@@ -2,6 +2,7 @@ import csv
 
 is_signedin = False
 
+#Check if the user is signed in or not
 def start_program():
     global is_signedin
     if is_signedin == True:
@@ -9,16 +10,18 @@ def start_program():
     if is_signedin == False:
         notsignedin()
 
+#the main menu if the user is signed in
 def signedin():
     print("Choose: \n    Change Password\n    Logout\n")
-    whichopt = input("  ")
-    match whichone:
+    whichoption = input("  ")
+    match whichoption:
         case "Change password"|"Change Password"|"change password":
             change_password()
         case "Logout"|"logout"|"Log out"|"log out"|"Log Out":
             print("You have logged out.")
             is_signedin = False
 
+#The main menu if the user is not signed in
 def notsignedin():
     print("Choose: \n    Login \n    Register \n    Quit")
     whichone = input("")
@@ -44,18 +47,18 @@ def create_account_username():
 
 #Check if the registered username already exists
 def doesusernamealreadyexist():
-    with open ("source.csv", "a") as file:
+    with open ("plain_text.txt", "a") as file:
         if username == row[0]:
             print("Username is already taken, please enter another.")
         else:
-            file.write(f"{username}, ")
+            file.write(f"{username},")
         create_account_password()
 
 
     #Create account - Password
 def create_account_password():
     password = input("Create Password: ")
-    with open ("source.csv", "a") as file:
+    with open ("plain_text.txt", "a") as file:
         file.write(f"{password}\n")
     login()
 
@@ -64,7 +67,6 @@ def create_account_password():
 #how??
 
 #LOGIN
-
 def login():
     global existingusername
     existingusername = input("Enter Username: ")
@@ -75,33 +77,42 @@ def login():
 def loginusernamechecker():
     usernamecheck = []
 
-    with open ("source.csv") as file:
+    with open ("plain_text.txt") as file:
         reader = csv.reader(file)
         for row in reader:
             usernamecheck.append({"username": row[0], "password": row[1]})
     if existingusername != row[0]:
         print("Username does not exist, try again.")
+        notsignedin()
     else: 
-        print(f"Username accepted, welcome, {existingusername}")
-        
+        print("Username accepted.")
+        loginpasswordchecker()
 
     #Is Password Correct?
 def loginpasswordchecker():
+    checkpassword = input("Enter Password: ")
     passwordcheck = []
 
-    with open ("source.csv") as file:
+    with open ("plain_text.txt") as file:
         for row in reader:
             passwordcheck.append({"password": row[1]})
     if passwordcheck != row[1]:
         print("Incorrect password, try again.")
+        notsignedin()
     else:
-        print("Password accepted, logged in.")
+        print(f"Password accepted, welcome, {existingusername}")
         is_signedin = True
+        signedin()
 
 #CHANGE PASSWORD
     #Edit source.csv to have the new password
 def change_password():
-    print("not working yet.")
+    newpassword = input("Enter new password: ")
+    with open("plain_text.txt", "a") as file:
+        writer = csv.DictWriter(file, fieldnames=["username", "password"])
+        writer.writerow({"username": username, "password": newpassword})
+    print("Password updated.")
+    is_signedin()
 
 #LOGOUT
     #End program
